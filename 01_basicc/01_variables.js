0adeecd3cab6d90e48ccfd9abf353a64a0fc452d
pisces2002 <108139886+pisces2002@users.noreply.github.com>
2024-01-16 17:52:16 +0000

    datatypes

---
+++ 01_basicc/01_variables.js
@@ -0,0 +1,22 @@
+const account_id=123456
+let account_Email="aryan@google.com"
+var account_password=4572189
+account_city="Jaipur"
+
+
+// const cannot be changed
+// account_id=234
+
+account_Email="akash@google.com"
+account_password=21212112
+account_city="Kolkata"
+console.log(account_id);
+console.table([account_Email,account_id,account_password,account_city])
+
+
+/* Prefer not to
+use var due to 
+block scope and functional scope */
+
+/* declare karke variable chor dene se
+javascript uski value undefined mannti hai */
---
+++ 01_basicc/02_datatypes.js
@@ -0,0 +1,13 @@
+"use strict";  // treat all JS code as newer version
+
+//  alert(3+3)   we are using node and not browser 
+
+/* number
+bigint
+string
+null => standalone value (typeof)
+undefined
+boolean
+symbol => unique
+
+*/
