f9c341702fd9ffa98378dcc712fca2fab1b29b0e
pisces2002 <108139886+pisces2002@users.noreply.github.com>
2024-01-17 06:39:31 +0000

    conversion of data types

---
+++ 01_basicc/conversion_operation.js
@@ -0,0 +1,18 @@
+let score="33"
+
+console.log(typeof score);
+
+let value=Number(score)
+console.log(typeof value)
+
+let score1="33abc"
+let value1=Number(score1)
+
+console.log(typeof value1);
+console.log(value1)
+
+let isLoggedIn=""
+let log_in=Boolean(isLoggedIn)
+console.log(log_in);
+
+//empty string are always false 
