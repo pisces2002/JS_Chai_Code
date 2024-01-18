9d38a614fbfd3db8b16f3d4b900d9f317e9c4fac
pisces2002 <108139886+pisces2002@users.noreply.github.com>
2024-01-18 09:19:20 +0000

    final_conversion

--- 01_basicc/conversion_operation.js
+++ 01_basicc/conversion_operation.js
@@ -16,3 +16,9 @@ let log_in=Boolean(isLoggedIn)
 console.log(log_in);
 
 //empty string are always false 
+console.log("1"+2);     //12
+console.log("1"+2+2);  //122
+console.log(1+2+"3"); //33
+
+console.log(true);
+console.log(+true); //console.log(true+) error dey dega
