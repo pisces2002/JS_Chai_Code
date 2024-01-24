445baa79e6a445e2be1117768001e46b57333d4c
pisces2002 <108139886+pisces2002@users.noreply.github.com>
2024-01-24 14:52:17 +0000

    comparison

---
+++ 01_basicc/04_comparison.js
@@ -0,0 +1,10 @@
+// console.log("2" > 1)
+// console.log("02" > 1)
+
+console.log(null==0);  //null kabhi 0 mie convert hota hai kabhi NaN mie
+console.log(null>=0);
+console.log(null<=0);
+console.log(null>0);
+console.log(null<0);
+
+//equality check and comparison operators work differently. Comparison operators convert null into 0 and then compare.
