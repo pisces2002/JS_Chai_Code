e6d725b85853a0d3f0fe8785b7ba8c8725baa853
pisces2002 <108139886+pisces2002@users.noreply.github.com>
2024-01-24 16:56:05 +0000

    datatype_Summary

---
+++ 01_basicc/datatypes_summary.js
@@ -0,0 +1,38 @@
+/* Primarily there are two types of data types
+
+
+Primitive: These are call by value data types.
+7 types: String, Number, Boolean, null, undefined, symbol(this is used to make values unique),BigInt
+
+const id= Symbol('123')
+const anotherId=Symbol('123')
+
+Still id != anotherId
+
+Reference(Non Primitive)
+Arrays, Functions, Objects
+
+let ObJ={
+    name:"Aryan",
+    age:21,
+}
+
+
+
+Primitive Datatypes
+       Number => number
+       String  => string
+       Boolean  => boolean
+       null  => object
+       undefined  =>  undefined
+       Symbol  =>  symbol
+       BigInt  =>  bigint
+
+2) Non-primitive Datatypes
+       Arrays  =>  object
+       Function  =>  function
+       Object  =>  object
+
+
+
+*/
